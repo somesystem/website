7a26094974e0bd3de640a68aeb1d5190236b4f53
zhe-he <hezhe@ihangmei.com>
2017-12-28 17:09:18 +0800

    add dist

---
+++ dist/js/chunk/newsDetail.js
@@ -0,0 +1,6 @@
+/*!
+ * 2017-12-28 17:08:17
+ * 
+ * built by `mufengkeji`
+ */
+webpackJsonp([4],{385:function(t,e,n){"use strict";function a(t){n(412)}Object.defineProperty(e,"__esModule",{value:!0});var i=n(391),s=n(414),r=n(46),c=a,o=r(i.a,s.a,!1,c,"data-v-1ff3699e",null);e.default=o.exports},391:function(t,e,n){"use strict";e.a={data:function(){return{}}}},412:function(t,e,n){var a=n(413);"string"==typeof a&&(a=[[t.i,a,""]]),a.locals&&(t.exports=a.locals),n(373)("1a28fd75",a,!0)},413:function(t,e,n){e=t.exports=n(132)(void 0),e.push([t.i,".news-detail[data-v-1ff3699e]{line-height:10;text-align:center}",""])},414:function(t,e,n){"use strict";var a=function(){var t=this,e=t.$createElement;return(t._self._c||e)("div",{staticClass:"news-detail"},[t._v("\n    新闻详情,新闻id: "+t._s(t.$route.params.id)+"\n")])},i=[],s={render:a,staticRenderFns:i};e.a=s}});
